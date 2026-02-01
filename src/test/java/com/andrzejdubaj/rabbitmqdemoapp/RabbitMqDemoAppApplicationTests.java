cea0ed98da396a09e2ae66fc3765602b9f06ace6
adubaj <dubaj.andrzej@gmail.com>
2026-02-01 01:16:25 +0100

    (test): mock messageListenerContainer to avoid RabbitMQ dependency in unit tests

--- src/test/java/com/andrzejdubaj/rabbitmqdemoapp/RabbitMqDemoAppApplicationTests.java
+++ src/test/java/com/andrzejdubaj/rabbitmqdemoapp/RabbitMqDemoAppApplicationTests.java
@@ -1,13 +1,18 @@
 package com.andrzejdubaj.rabbitmqdemoapp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.listener.MessageListenerContainer;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RabbitMqDemoAppApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @MockBean(name = "messageListenerContainer")
+    MessageListenerContainer messageListenerContainer;
 
+    @Test
+    void contextLoads() {}
 }
